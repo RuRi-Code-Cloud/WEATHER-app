d2c72105bd788aba733d2ddd3b6c48cb97400dbb
RuRi-Code-Cloud <rudziter367@gmail.com>
2023-06-27 21:56:17 +0100

    added new file

---
+++ week.7 Weather-app/src/app.js
@@ -0,0 +1,52 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function showTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let countryElement = document.querySelector("#country");
+  countryElement.innerHTML = response.data.country;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let weatherElement = document.querySelector("#type");
+  weatherElement.innerHTML = response.data.condition.description;
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatDate(response.data.time * 1000);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    `src`,
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+}
+
+let apiKey = "2b03bfeb040ctdb92faf2af53622202o";
+let city = "Dublin";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=${city}&key=2b03bfeb040ctdb92faf2af53622202o";
+
+axios.get(apiUrl).then(showTemperature);
